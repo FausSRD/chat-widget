b2aa559beaa8b67dd0a2964511d246715548c033
FausSR <faustosantinelli@gmail.com>
2025-06-25 06:13:22 +0200

    test

--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 import os
-print("✅ Este mensaje confirma que se está ejecutando server.py")
-
-PORT = int(os.environ.get("PORT", 8000))
-print(f"Puerto detectado desde entorno: {PORT}")
-
 from http.server import SimpleHTTPRequestHandler
 import socketserver
 
+PORT = int(os.environ.get("PORT", 8000))
 Handler = SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
     print(f"🚀 Servidor iniciado en el puerto {PORT}")
     httpd.serve_forever()
