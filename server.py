e78d98d7925ce2c07ad70f564b537d0109406750
FausSR <faustosantinelli@gmail.com>
2025-06-25 06:03:56 +0200

    test

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-import os
-from http.server import SimpleHTTPRequestHandler
+import http.server
 import socketserver
+import os
 
+# Railway asigna el puerto en esta variable
 PORT = int(os.environ.get("PORT", 8000))
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving at port {PORT}")
+    print(f"🚀 Servidor iniciado en el puerto {PORT}")
     httpd.serve_forever()
