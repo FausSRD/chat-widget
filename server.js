0b43a498f224b7856946a26fd7fd7f0362ab0eaa
FausSR <faustosantinelli@gmail.com>
2025-09-13 23:24:04 +0200

    Add internal company widget

--- server.js
+++ server.js
@@ -19,6 +19,20 @@ app.get('/chat-widget.js', (req, res) => {
   }
 });
 
+app.get('/internat-widget-company.js', (req, res) => {
+  const acceptEncoding = req.headers['accept-encoding'] || '';
+  const basePath = path.join(__dirname, 'internat-widget-company.js');
+
+  if (acceptEncoding.includes('gzip') && fs.existsSync(basePath + '.gz')) {
+    res.setHeader('Content-Encoding', 'gzip');
+    res.setHeader('Content-Type', 'application/javascript');
+    fs.createReadStream(basePath + '.gz').pipe(res);
+  } else {
+    res.setHeader('Content-Type', 'application/javascript');
+    fs.createReadStream(basePath).pipe(res);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
