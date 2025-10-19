852004f7a40c9b71cd5428c9613f846e74f71457
FausSR <faustosantinelli@gmail.com>
2025-10-19 13:50:19 +0200

    Fix

--- internal-widget-company.js
+++ internal-widget-company.js
@@ -1,8 +1,8 @@
 (function () {
     const configSource = window.ChatWidgetConfig || {}
     const config = {
-        webhookUrl: 'https://leadhookai-pre.up.railway.app/app-backend-api/v1/internal-widget-company/chat',
-        // webhookUrl: 'http://localhost:8080/app-backend-api/v1/internal-widget-company/chat',
+        // webhookUrl: 'https://leadhookai-pre.up.railway.app/app-backend-api/v1/internal-widget-company/chat',
+        webhookUrl: 'http://localhost:8080/app-backend-api/v1/internal-widget-company/chat',
         title: configSource.title || 'Internal Support Assistant',
         welcomeMessage: configSource.welcomeMessage || "Hi! I'm your virtual assistant. How can i help you?",
         quickReplies: configSource.quickReplies || ['Option 1', 'Option 2'],
@@ -199,6 +199,7 @@
             border-radius: 18px;
             line-height: 1.4;
             word-wrap: break-word;
+            font-size: 14px;
         }
 
         .lh-bot-message {
@@ -335,11 +336,13 @@
             color: #333;
             border: none;
             border-radius: 15px;
-            padding: 8px 15px;
             cursor: pointer;
             font-size: 14px;
             transition: background-color 0.3s ease;
             box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            font-weight: normal;
+            padding: 10px 15px;
+            line-height: 1.4;
         }
 
         .lh-quick-reply-button:hover {
